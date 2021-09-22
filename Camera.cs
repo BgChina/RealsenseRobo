70ffb13039dd81f782f65fd42f1f3667ff9e4d3a
colee <colee@DESKTOP-3O4S6T6>
2021-09-21 21:43:24 -0400

    Add project files.

---
+++ Camera.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intel.RealSense;
+
+namespace RealsenseTest
+{
+    class Camera
+    {
+        public float checkDepth()
+        {
+            var pipe = new Pipeline();
+            pipe.Start();
+
+            while (true)
+            {
+                using var frames = pipe.WaitForFrames();
+                using var depth = frames.DepthFrame;
+                Console.SetCursorPosition(0, 0);
+
+                return depth.GetDistance(depth.Width / 2, depth.Height / 2);
+            }
+
+
+        }
+        public bool distCheck(float depth)
+        {
+
+            if (depth <= 0.5)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ ConsoleMain.cs
@@ -0,0 +1,24 @@
+﻿using Intel.RealSense;
+using System;
+
+namespace RealsenseTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Camera Camera1 = new();
+
+            while (true)
+            {
+                Console.WriteLine("The camera is pointing at an object " +
+                    Camera1.checkDepth() + " meters away\t");
+
+                if (Camera1.distCheck(Camera1.checkDepth()) == false)
+                    Console.WriteLine("The camera is not far enough");
+                else
+                    Console.WriteLine("The camera is far enough");
+            }
+        }
+    }
+}
